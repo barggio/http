f21f84cab9ff4029f1f2e8927e1f0baa8a811dae
Nopparit Intharasombat <nopparit.intharasombat@mckesson.com>
2017-07-27 20:22:04 +0000

    7/27/2017 13:00

--- finance.rb
+++ finance.rb
@@ -1,12 +1,14 @@
 require 'httparty'
 require 'pry'
+require 'etc'
 #require 'json'
 print 'Enter your stock symbol:'
 in_symbol = gets 
 ticker = in_symbol.chomp.upcase
+p Etc.getlogin 
 
 url = 'https://www.google.com/finance/getprices?q=' + ticker.to_s + '&x=NASD&i=120&p=25m&f=d,c,v,o,h,l&df=cpct&auto=1&ts=1501179683973&ei=vCl6WbGZHNSP2Abo3KPoDg'
-while (1)
+while true
     content = (HTTParty.get url).body
     lines = content.split("\n")
     
